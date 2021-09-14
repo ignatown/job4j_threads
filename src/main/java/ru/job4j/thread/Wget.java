08ab77b815e01f60f5c5e8eefed3b3f93d51a10b
ignatown <ignatow.n@mail.ru>
2021-09-14 14:27:56 +0300

    fix 4. Скачивание файла с ограничением. [#144271 #202795]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -8,26 +8,29 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String fileName;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            var startTime = System.nanoTime();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                var startTime = System.nanoTime();
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
                 var stopTime = System.nanoTime();
                 var dTime = stopTime - startTime;
                 if (dTime < speed) {
                     Thread.sleep(speed - dTime);
                 }
+                startTime = System.nanoTime();
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -35,12 +38,13 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        if (args.length < 2) {
+        if (args.length != 3) {
             throw new IllegalArgumentException();
         }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String fileName = args[2];
+        Thread wget = new Thread(new Wget(url, speed, fileName));
         wget.start();
         wget.join();
     }
