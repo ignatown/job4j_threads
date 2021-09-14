49270c060eb68adaf1b0abe7fdade0c7ed0859d9
ignatown <ignatow.n@mail.ru>
2021-09-14 14:14:07 +0300

    fix 1.1. Состояние нити. [#229175 #202723]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -11,7 +11,7 @@ public class ThreadState {
         System.out.println(first.getName() + ": " + first.getState());
         System.out.println(second.getName() + ": " + first.getState());
         first.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getName() + ": " + first.getState());
             System.out.println(second.getName() + ": " + first.getState());
         }
