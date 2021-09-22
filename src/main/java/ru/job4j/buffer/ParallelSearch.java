d732e6ee4c6e8d47a1ecc373da54c745584d6055
ignatown <ignatow.n@mail.ru>
2021-09-23 01:37:02 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,36 @@
+package ru.job4j.buffer;
+
+import ru.job4j.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        producer.join();
+        consumer.join();
+    }
+}
