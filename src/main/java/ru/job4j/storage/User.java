5f0f81ac9f384783eead958f02d9046814968053
ignatown <ignatow.n@mail.ru>
2021-09-23 00:46:39 +0300

    fix 3. Класс хранилища пользователей UserStorage [#1104 #203809]

--- src/main/java/ru/job4j/storage/User.java
+++ src/main/java/ru/job4j/storage/User.java
@@ -4,6 +4,11 @@ public class User {
     private int id;
     private int amount;
 
+    public User(int id, int amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
     public int getId() {
         return id;
     }
--- src/main/java/ru/job4j/storage/UserStorage.java
+++ src/main/java/ru/job4j/storage/UserStorage.java
@@ -9,14 +9,14 @@ import java.util.Map;
 @ThreadSafe
 public class UserStorage {
     @GuardedBy("this")
-    private final Map<Integer, Integer> userMap = new HashMap<Integer, Integer>();
+    private final Map<Integer, User> userMap = new HashMap<>();
 
     private synchronized boolean add(User user) {
-        return userMap.putIfAbsent(user.getId(), user.getAmount()) == null;
+        return userMap.putIfAbsent(user.getId(), user) == null;
     }
 
     private synchronized boolean update(User user) {
-        return userMap.replace(userMap.get(user.getId()), userMap.get(user.getAmount()), user.getAmount());
+        return userMap.replace(user.getId(), userMap.get(user), new User(user.getId(), user.getAmount()));
     }
 
     public synchronized boolean delete(User user) {
@@ -25,8 +25,8 @@ public class UserStorage {
 
     private synchronized boolean transfer(int fromId, int toId, int amount) {
         if (userMap.containsKey(fromId) && userMap.containsKey(toId)) {
-            userMap.put(fromId, userMap.get(fromId) - amount);
-            userMap.put(toId, userMap.get(toId) + amount);
+            userMap.put(fromId, new User(fromId, userMap.get(fromId).getAmount() - amount));
+            userMap.put(toId, new User(toId, userMap.get(toId).getAmount() + amount));
             return true;
         }
         return false;
