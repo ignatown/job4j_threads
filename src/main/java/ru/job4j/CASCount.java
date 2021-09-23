0dc77ea94e9a3b80b505b4b6f459fcabc720f9a1
ignatown <ignatow.n@mail.ru>
2021-09-23 19:52:53 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/CASCount.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+import net.jcip.annotations.ThreadSafe;
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>(0);
+
+    public void increment() {
+        int current;
+        int next;
+        do {
+            current = count.get();
+            next = current + 1;
+        } while (!count.compareAndSet(current, next));
+    }
+    public int get() {
+        return count.get();
+    }
+}
